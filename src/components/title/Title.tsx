f37a929f4b290f376641a75cc9d1631473ad6164
Alejandro Sanchez <sanchezgalejandroj@gmail.com>
2023-11-04 19:41:00 +0100

    feat: :recycle: reorganizing folder structure
    moving components to differents folders, separating components and logic

---
+++ src/components/title/Title.tsx
@@ -0,0 +1,10 @@
+import "./Title.css";
+
+export const Title = () => {
+  return (
+    <article className="layout__title">
+      <h1>lista de personajes</h1>
+      <hr />
+    </article>
+  );
+};
