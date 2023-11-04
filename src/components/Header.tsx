8008209c2c8bbcd7f90bcbc7dcfb602a3b934b18
Alejandro Sanchez <sanchezgalejandroj@gmail.com>
2023-11-04 15:55:18 +0100

    feat: :sparkles: providing app with css rules to adjust to every resolution

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,16 @@
 import logo from "../assets/Rick_and_Morty.svg.png";
+import logoMobile from "../assets/mobileHeaderLogo.png";
 import "./Header.css";
 
 export const Header = () => {
   return (
     <header>
-      <img src={logo} alt="Rick and Morty Logo" />
+      <img
+        src={logoMobile}
+        className="header__logo-mobile"
+        alt="Rick and Morty Logo"
+      />
+      <img src={logo} className="header__logo" alt="Rick and Morty Logo" />
     </header>
   );
 };
